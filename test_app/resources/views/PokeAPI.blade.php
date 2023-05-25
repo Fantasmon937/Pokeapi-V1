82d88aa8f22ff43f0bda9ef8e6c2fa14958030b7
Fantasmon937 <pablo991004.lalo@gmail.com>
2023-05-25 13:37:37 -0600

    Created Proyect on Laravel

--- test_app/resources/views/PokeAPI.blade.php
+++ test_app/resources/views/PokeAPI.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Pokedex</title>
 
-    <link rel="stylesheet" href="PokeAPI.css" />
+    <link rel="stylesheet" href="{{asset('css/PokeAPI.css')}}" />
 
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
@@ -21,7 +21,7 @@
       integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
       crossorigin="anonymous"
     ></script>
-    <script src="PokeAPI.js"></script>
+    <script src="{{asset('js/PokeAPI.js')}}"></script>
     <div class="content">
       <div
         id="content_box"
